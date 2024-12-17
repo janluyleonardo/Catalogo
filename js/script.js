438bb78c995c9341c8bedd82f574e7606157632a
janluyleonardo <janluy.moreno@gmail.com>
2024-12-17 17:34:26 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('data/products.json')
+        .then(response => response.json())
+        .then(data => {
+            const productList = document.getElementById('product-list');
+            data.products.forEach(product => {
+                const productItem = document.createElement('div');
+                productItem.classList.add('product-item');
+                productItem.innerHTML = `
+                    <img src="images/${product.image}" alt="${product.name}">
+                    <h2>${product.name}</h2>
+                    <p>${product.description}</p>
+                    <p>Precio: $${product.price}</p>
+                `;
+                productList.appendChild(productItem);
+            });
+        })
+        .catch(error => console.error('Error cargando los productos:', error));
+});
